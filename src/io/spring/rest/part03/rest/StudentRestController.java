e8df212236d5ecfdc132f6dda5a50d0cd8f9310c
tutkuince <tutku.ince@outlook.com>
2018-08-13 19:18:11 +0300

    if statement for studentId field

--- src/io/spring/rest/part03/rest/StudentRestController.java
+++ src/io/spring/rest/part03/rest/StudentRestController.java
@@ -38,6 +38,11 @@ public class StudentRestController {
 	// define endpoint for "/students/{studentId} - return Student object
 	@GetMapping("/students/{studentId}")
 	public Student getStudent(@PathVariable int studentId) {
+		
+		// check the studentId against list size
+		if((studentId >= studentList.size()) || (studentId < 0))
+			throw new StudentNotFoundException("Student id not found - " + studentId); 
+		
 		return studentList.get(studentId);
 	}
 }
