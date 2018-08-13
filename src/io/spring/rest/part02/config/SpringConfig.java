1de3047a0bf21d076cc61fd37b4f3163a679a5c2
tutkuince <tutku.ince@outlook.com>
2018-08-13 17:10:35 +0300

    spring configuration file created

---
+++ src/io/spring/rest/part02/config/SpringConfig.java
@@ -0,0 +1,24 @@
+package io.spring.rest.part02.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("io.spring.rest.part02")
+public class SpringConfig implements WebMvcConfigurer {
+
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+
+		return viewResolver;
+	}
+}
