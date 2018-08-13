90f423abd68bb46334a24fe68412768806000c1d
tutkuince <tutku.ince@outlook.com>
2018-08-13 16:32:50 +0300

    Rest over Http notes

---
+++ src/io/spring/rest/part02/RestOverHTTPNotes.java
@@ -0,0 +1,16 @@
+package io.spring.rest.part02;
+
+public class RestOverHTTPNotes {
+	/**
+	 * Most common use of REST is over HTTP
+	 * 
+	 * Leverage HTTP methods for CRUD operations
+	 * 
+	 * HTTP Method			CRUD Operation
+	 * 
+	 * POST					Create a new entity
+	 * GET					Read a list of entities or single entity
+	 * PUT					Update an existing entity
+	 * DELETE				Delete an existing entity
+	 * */
+}
