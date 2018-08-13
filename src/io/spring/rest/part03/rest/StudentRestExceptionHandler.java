25e63dfa48f2aeabc77053aec40380f74126ddd3
tutkuince <tutku.ince@outlook.com>
2018-08-13 19:42:13 +0300

    added info about @ControlAdvice

--- src/io/spring/rest/part03/rest/StudentRestExceptionHandler.java
+++ src/io/spring/rest/part03/rest/StudentRestExceptionHandler.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice
+@ControllerAdvice	// global exception
 public class StudentRestExceptionHandler {
 
 	// add exception handing code here
