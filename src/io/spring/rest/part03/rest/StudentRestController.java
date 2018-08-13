73aa00f2826c34144cec3f55e2b5311b544a1353
tutkuince <tutku.ince@outlook.com>
2018-08-13 18:53:35 +0300

    refactoring with @PostConstructor

--- src/io/spring/rest/part03/rest/StudentRestController.java
+++ src/io/spring/rest/part03/rest/StudentRestController.java
@@ -3,6 +3,8 @@ package io.spring.rest.part03.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,17 +15,22 @@ import io.spring.rest.part03.model.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 
-	// define endpoint for "/students" - return list of students
-	@GetMapping("/students")
-	public List<Student> getStudents() {
+	private List<Student> studentList;
 
+	// define @PostConstructor to load the student data. . . only once!
+	@PostConstruct
+	public void loadData() {
 		// sample students
-		List<Student> studentList = new ArrayList<>();
+		studentList = new ArrayList<>();
 		studentList.add(new Student("Tutku", "Ince"));
 		studentList.add(new Student("Utku", "Ince"));
 		studentList.add(new Student("Emin", "Koklu"));
 		studentList.add(new Student("Ugur", "Batikan"));
+	}
 
+	// define endpoint for "/students" - return list of students
+	@GetMapping("/students")
+	public List<Student> getStudents() {
 		return studentList;
 	}
 }
