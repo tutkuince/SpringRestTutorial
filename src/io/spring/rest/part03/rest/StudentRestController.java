7e4d271bb313fcea1f60464dcdf6fa316869dfc0
tutkuince <tutku.ince@outlook.com>
2018-08-13 18:57:03 +0300

    getStudent method

--- src/io/spring/rest/part03/rest/StudentRestController.java
+++ src/io/spring/rest/part03/rest/StudentRestController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,5 +33,11 @@ public class StudentRestController {
 	@GetMapping("/students")
 	public List<Student> getStudents() {
 		return studentList;
+	}
+
+	// define endpoint for "/students/{studentId} - return Student object
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+		return getStudents().get(studentId);
 	}
 }
